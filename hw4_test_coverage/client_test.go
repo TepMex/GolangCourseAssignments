ca2c80c9b04c81fff526759d0dd8ce344eb977eb
Mikhail Semikolenov <mihawww@yandex.ru>
2019-09-12 23:05:57 +0600

    hw4 beginning

--- hw4_test_coverage/client_test.go
+++ hw4_test_coverage/client_test.go
@@ -1,3 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SearchServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%v", r)
+}
+
 // код писать тут
+func TestServer(t *testing.T) {
+	testReq := SearchRequest{
+		Limit:      10,
+		Offset:     1,
+		Query:      "male",
+		OrderField: "gender",
+		OrderBy:    OrderByAsc,
+	}
+
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	testClient := SearchClient{
+		URL:         testServer.URL,
+		AccessToken: "ZHOPA",
+	}
+
+	testClient.FindUsers(testReq)
+}
